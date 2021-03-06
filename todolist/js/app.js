718e950890d02aec6a8de926ab5b7b9f18c5ca67
Matteo Greco <47497176+grecomatteo@users.noreply.github.com>
2021-03-06 12:05:12 +0100

    update_06/03/2021

--- todolist/js/app.js
+++ todolist/js/app.js
@@ -4,13 +4,31 @@ var numActivities = 0;
 function addActivity(){
     // catturo l'elemento html dall'input
     let nActInp = document.getElementById("newActivity");
+    
     if (nActInp.value.trim() != ""){
-        // creazione elemento LI
+        // creazione dei vari elementi necessari
         let nActItem = document.createElement("li");
-        // popoliamo l'elemento LI
-        nActItem.innerHTML = nActInp.value;
+        let nActImg = document.createElement("img");
+        let nActSpan = document.createElement("span");
+
+        // qui imposto i parametri dell'immagine
+        nActImg.src = "img/btn_delete.png";
+        nActImg.onclick = function(event){
+            // puntando al padre vado all'elemento LI, in posizione quindi superiore all'img
+            let myList = event.target.parentNode;
+            // ora poi punto al padre successiva che sarebbe tutta la lista
+            // e faccio il remove dalla lista completa della lista ottenuta prima
+            myList.parentNode.removeChild(myList);
+            //aggiorno il contatore decrementando
+            numActivities--;
+            document.getElementById("numActivities").innerHTML = numActivities;
+        }
+
+        // qui imposto lo span
+        nActSpan.innerHTML = nActInp.value;
+
         // aggiungo anche l'evento click
-        nActItem.onclick = function() {
+        nActSpan.onclick = function() {
             if (this.classList.contains("done")){
                 this.classList.remove("done");
                 this.classList.add("todo");
@@ -19,12 +37,20 @@ function addActivity(){
                 this.classList.add("done");
             }     
         }
-        // incrementiamo il contatore 
-        numActivities++;
-        //mettiamo il contatore nell'html
-        document.getElementById("numActivities").innerHTML = numActivities;
+
+        // devo aggiungere i due figli al list item
+        nActItem.appendChild(nActImg);
+        nActItem.appendChild(nActSpan);
+
         // aggiungo elemento alla lista "listActivity"
         document.getElementById("listActivity").appendChild(nActItem);
+
+        // incrementiamo il contatore 
+        numActivities++;
+
+        //mettiamo il contatore nell'html
+        document.getElementById("numActivities").innerHTML = numActivities;
+
         //pulisco casella input
         nActInp.value = "";    
     }
@@ -33,15 +59,4 @@ function clearList(){
     numActivities = 0;
     document.getElementById("numActivities").innerHTML = numActivities;
     document.getElementById("listActivity").innerHTML = "";
-}
-function setItem(activity){
-  
-    if (activity.classList.contains("done")){
-        activity.classList.remove("done");
-        activity.classList.add("todo");
-    } else {
-        activity.classList.remove("todo");
-        activity.classList.add("done");
-    }
-
 }
